1b23c2173039ac8a659882dc0aa1e3fa1cf01293
Carmine Paolino <earcar@gmail.com>
2009-02-26 12:30:56 +0100

    OMG I forgot error.h!

---
+++ include/error.h
@@ -0,0 +1,19 @@
+#define ERR_SELECT 2, "select()"
+#define ERR_MONPACK 3, "Monitor packet not understood"
+#define ERR_NOFDSET 4, "Incoming data, but in no controlled fd. Strange"
+#define ERR_TRANSPORT 5, "Invalid transport"
+#define ERR_READZERO 6, "Nothing received, maybe the other end is down? Exiting."
+#define ERR_RECV 7, "failed reading %d bytes from socket %d", n, socketfd
+#define ERR_SEND 8, "failed writing %d bytes from socket %d", size, socketfd
+#define ERR_GETTIMEOFDAY 10, "gettimeofday()"
+#define ERR_INETATON 11, "inet_aton() error (addr %s, port %d)", addr, port
+#define ERR_SOCKET 12, "socket() error (type %d)", type
+#define ERR_SETSOCKOPT 13, "setsockopt() error (type %d, socketfd %d)", type, socketfd
+#define ERR_BIND 14, "bind() error (port %d, socketfd %d)", port, socketfd
+#define ERR_LISTEN 15, "listen() error (port %d, socketfd %d)", port, socketfd
+#define ERR_CONNECT 16, "connect() error (port %d, socketfd %d)", port, socketfd
+#define ERR_ACCEPT 17, "accept() error (socketfd %d)", socketfd
+#define ERR_MALLOC 18, "malloc()"
+
+/* WARNINGS */
+#define WARN_2PKTQ "Oh my god two packets with the same id %u in Queue!", packet->id
